be57c5c70fe1cbd5e94c9a340ce429dfe247a3a4
Michael Afrin <1360790+mkafrin@users.noreply.github.com>
2019-11-01 23:48:22 -0400

    Update index.js

--- server/routes/holidays/index.js
+++ server/routes/holidays/index.js
@@ -4,10 +4,10 @@ const express = require('express'),
 const router = express.Router();
 
 // Creates a route that starts at the root of THIS router, and passes the request to the 'holidays.getAll' function in our controller
-// Note I said THIS router. In app.js, we pass this router to any route that start with /api/holidays, so any routes defined here
+// Note I said THIS router. In app.js, we pass this router to any route that starts with /api/holidays, so any routes defined here
 // will come AFTER that route. '/' in this case corresponds to '/api/holidays/'
 router.get('/', holidays.getAll);
 
 
-// We are exporting the router so that in our app.js, when this file is imported(require), the router will be returned
+// We are exporting the router so that in our app.js, when this file is imported, the router will be returned
 module.exports = router;
