1aee83551d3e286f7f45a4f28bd61a5437f2f1ef
Michael Afrin <1360790+mkafrin@users.noreply.github.com>
2019-11-02 00:02:08 -0400

    Update index.js

--- server/services/holiday/index.js
+++ server/services/holiday/index.js
@@ -1,6 +1,5 @@
 const dayjs = require('dayjs');
 const Holiday = require('./holiday');
-// Helper objects (enums basically) so we can use the names of months and days instead of numbers
 const MONTH = {January: 0, February: 1, March: 2, April: 3, May: 4, June: 5, July: 6, August: 7, September: 8, October: 9, November: 10, December: 11};
 const DAY = {Sunday: 0, Monday: 1, Tuesday: 2, Wednesday: 3, Thursday: 4, Friday: 5, Saturday: 6};
 
